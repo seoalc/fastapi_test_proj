36285db4cffe7a639845caf6e046f62f174d0366
unknown <rrryyy07@gmail.com>
2025-02-13 08:51:39 +0500

    initial commit

--- generate_tron.py
+++
@@ -1,7 +0,0 @@
-from tronpy import Tron
-
-client = Tron()
-new_wallet = client.generate_address()
-print(new_wallet)
-# print("Private Key:", new_wallet["private_key"])
-# print("Public Address:", new_wallet["address"])
